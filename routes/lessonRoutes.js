3750280e1daa41b800dfc41fe32a54137b66c6e9
User <vahran56@gmail.com>
2023-10-06 02:00:22 +0700

    Lesson list & Team

--- routes/lessonRoutes.js
+++ routes/lessonRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { lessonController } = require("../controllers");
 const router = express.Router();
 
-// get all books
 router.get("/lessons", lessonController.getLessons);
 router.post("/lessons", lessonController.createLesson);
 
